a692e5c6dc810dc121c4f406534e88730276afca
Deenassalam <38691586+Deenassalam@users.noreply.github.com>
2018-12-18 00:53:08 +0700

    siap expo
    file fix untuk expo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,16 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	public function index()
+	public function index1()
 	{
 		$this->load->view('Menu');
 	}
-	public function form_input(){
-		$this->load->view('Form_daftar');
-	}
-	public function form_login(){
-		$this->load->view('login');
-	}
-	
-	
 }
